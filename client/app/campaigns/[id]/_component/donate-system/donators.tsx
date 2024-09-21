772c80721c3978b1d5e66d0ae9a318a70260a3c3
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-09-21 18:24:51 +0100

    Show empty donors message

--- client/app/campaigns/[id]/_component/donate-system/donators.tsx
+++ client/app/campaigns/[id]/_component/donate-system/donators.tsx
@@ -8,23 +8,37 @@ interface DonatorsProps {
 }
 
 export default function Donators({ donors }: DonatorsProps) {
+  const donorsList = donors.data && Object.entries(donors.data);
+
   return (
     <section className="bg-gray-100 py-4 rounded-lg">
       <h2 className="px-4">Donors</h2>
       <FetchedData item={donors}>
-        {donors.data && (
-          <ul className="px-4 overflow-y-auto max-h-[20rem] flex flex-col space-y-2">
-            {Object.entries(donors.data).map(([donor, amount]) => {
-              return (
-                <li key={donor} className="flex items-center justify-between">
-                  <span title={donor} className="text-gray-500 font-semibold">
-                    {formatAddress(donor)}
-                  </span>
-                  <span>{amount}</span>
-                </li>
-              );
-            })}
-          </ul>
+        {donorsList && (
+          <>
+            {donorsList.length === 0 ? (
+              <p className="px-4">There are no donors yet!</p>
+            ) : (
+              <ul className="px-4 overflow-y-auto max-h-[20rem] flex flex-col space-y-2">
+                {donorsList.map(([donor, amount]) => {
+                  return (
+                    <li
+                      key={donor}
+                      className="flex items-center justify-between"
+                    >
+                      <span
+                        title={donor}
+                        className="text-gray-500 font-semibold"
+                      >
+                        {formatAddress(donor)}
+                      </span>
+                      <span>{amount}</span>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+          </>
         )}
       </FetchedData>
     </section>
