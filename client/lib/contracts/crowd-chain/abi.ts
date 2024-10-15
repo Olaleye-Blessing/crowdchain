cfae43a81c0a1d57bdcee93eee18251b1fb113e3
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-10-15 12:50:54 +0100

    Update client ABI to conform with contract ABI

--- client/lib/contracts/crowd-chain/abi.ts
+++ client/lib/contracts/crowd-chain/abi.ts
@@ -1,4 +1,4 @@
-const campaignDetails = `tuple(uint256 id, uint256 amountRaised, uint256 deadline, uint256 refundDeadline, uint256 goal, address owner, string title, string description, string coverImage, bool claimed, uint256 totalDonors, uint256 tokensAllocated, uint8 totalMilestones, uint8 currentMilestone, uint8 nextWithdrawableMilestone)`;
+const campaignDetails = `tuple(uint256 id, uint256 amountRaised, uint256 deadline, uint256 refundDeadline, uint256 goal, uint256 totalDonors, uint256 tokensAllocated, uint8 totalMilestones, uint8 currentMilestone, uint8 nextWithdrawableMilestone, address owner, string title, string description, string coverImage, bool claimed)`;
 
 const basicMilestone = `tuple(uint256 targetAmount, uint256 deadline, string description)`;
 
@@ -8,7 +8,7 @@ export const crowdChainABI = [
   `function createCampaign(string title, string description, string coverImage, ${basicMilestone}[] milestones, uint256 goal, uint256 duration, uint256 refundDeadline)`,
   "function totalCampaigns() public view returns (uint256)",
   `function getCampaigns(uint256 page, uint256 perPage) external view returns (${campaignDetails}[], uint256)`,
-  `function getOwnerCampaigns(address owner, uint256 page, uint256 perPage) external view returns (${campaignDetails}[] memory, uint256)`,
+  `function getOwnerCampaigns(address owner, uint256 page, uint256 perPage) external view returns (${campaignDetails}[], uint256)`,
   `function getCampaign(uint256 campaignId) view returns (${campaignDetails})`,
   `function getCampaignMileStones(uint256 campaignId) view returns (${milestone}[], uint8, uint8)`,
   "function donate(uint256 campaignId) public payable",
