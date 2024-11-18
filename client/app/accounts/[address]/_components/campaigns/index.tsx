b3cf48d27adf2a561fb5dfd311bec45f2d8df683
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-11-18 11:42:23 +0100

    Refactor paginated campaigns into a component

--- client/app/accounts/[address]/_components/campaigns/index.tsx
+++ client/app/accounts/[address]/_components/campaigns/index.tsx
@@ -8,16 +8,14 @@ import { useCrowdchainAddress } from "@/hooks/use-crowdchain-address";
 import { IAddress } from "@/interfaces/address";
 import { ICampaignDetail } from "@/interfaces/campaign";
 import { wagmiAbi } from "@/lib/contracts/crowd-chain/abi";
-import Campaign from "@/components/campaigns/campaign";
-import Loading from "@/app/loading";
-import { Button } from "@/components/ui/button";
+import PaginatedCampaigns from "@/components/campaigns/paginated";
 
 const perPage = process.env.NODE_ENV === "production" ? "20" : "2";
 
 export default function Campaigns({ account }: { account: IAddress }) {
   const [page, setPage] = useState(0);
-  const [totalPages, setTotalPages] = useState<number | null>(null);
-  const [campaigns, setCampaigns] = useState<ICampaignDetail[] | null>(null);
+  const [totalCampaigns, setTotalCampaigns] = useState<number | null>(null);
+  const [campaigns, setCampaigns] = useState<ICampaignDetail[]>([]);
   const { data, isFetching, error } = useReadContract({
     abi: wagmiAbi,
     address: useCrowdchainAddress(),
@@ -35,50 +33,24 @@ export default function Campaigns({ account }: { account: IAddress }) {
 
     const [_campaigns, _total] = data;
 
-    if (_campaigns.length === 0) return;
+    if (_campaigns.length === 0)
+      return setTotalCampaigns(+formatUnits(_total, 0));
 
     setCampaigns(_campaigns.map((cam) => constructCampaign(cam)));
 
-    if (!totalPages) setTotalPages(+formatUnits(_total, 0));
+    if (!totalCampaigns) setTotalCampaigns(+formatUnits(_total, 0));
 
     return () => {};
   }, [data]);
 
   return (
-    <>
-      {!isFetching && !error && !campaigns && (
-        <p className="text-center">No Campaigns</p>
-      )}
-      {campaigns && (
-        <ul className="mb-4 grid gap-4 grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(18rem,_1fr))] md:grid-cols-[repeat(auto-fit,minmax(21rem,_1fr))]">
-          {campaigns.map((cam: any) => (
-            <Campaign
-              key={cam.id}
-              campaign={cam}
-              className="max-w-96 md:max-w-[28rem]"
-            />
-          ))}
-        </ul>
-      )}
-      {isFetching && <Loading />}
-      {error && <p className="text-red-800">Error</p>}
-      {Boolean(
-        !isFetching &&
-          !error &&
-          totalPages &&
-          campaigns &&
-          campaigns.length < totalPages,
-      ) && (
-        <Button
-          onClick={() => {
-            setPage((prev) => prev + 1);
-          }}
-          className="block mx-auto"
-          variant="secondary"
-        >
-          Load More
-        </Button>
-      )}
-    </>
+    <PaginatedCampaigns
+      totalCampaigns={totalCampaigns}
+      campaigns={campaigns}
+      isFetching={isFetching}
+      error={error}
+      emptyCampaignsMsg="No campaigns"
+      loadMore={() => setPage((prev) => prev + 1)}
+    />
   );
 }
---
+++ client/components/campaigns/paginated.tsx
@@ -0,0 +1,57 @@
+import { ReadContractErrorType } from "viem";
+import { ICampaignDetail } from "@/interfaces/campaign";
+import Loading from "@/app/loading";
+import Campaign from "./campaign";
+import { Button } from "../ui/button";
+
+interface PaginatedCampaignsProps {
+  totalCampaigns: number | null;
+  campaigns: ICampaignDetail[];
+  isFetching: boolean;
+  error: ReadContractErrorType | null;
+  loadMore(): void;
+  emptyCampaignsMsg: string;
+}
+
+export default function PaginatedCampaigns({
+  totalCampaigns,
+  campaigns,
+  isFetching,
+  error,
+  emptyCampaignsMsg,
+  loadMore,
+}: PaginatedCampaignsProps) {
+  return (
+    <>
+      {Boolean(totalCampaigns) && (
+        <ul className="grid gap-4 grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(18rem,_1fr))] md:grid-cols-[repeat(auto-fit,minmax(21rem,_1fr))]">
+          {campaigns.map((cam) => (
+            <Campaign
+              key={cam.id}
+              campaign={cam}
+              className="max-w-96 md:max-w-[28rem]"
+            />
+          ))}
+        </ul>
+      )}
+      {!isFetching && !error && totalCampaigns === 0 && (
+        <p className="text-center">{emptyCampaignsMsg}</p>
+      )}
+      <div className="flex items-center justify-center mt-4">
+        {error && <p>error</p>}
+        {isFetching && <Loading />}
+        {totalCampaigns !== null &&
+          !isFetching &&
+          campaigns.length < totalCampaigns && (
+            <Button
+              onClick={loadMore}
+              className="block mx-auto"
+              variant="secondary"
+            >
+              Load More
+            </Button>
+          )}
+      </div>
+    </>
+  );
+}
