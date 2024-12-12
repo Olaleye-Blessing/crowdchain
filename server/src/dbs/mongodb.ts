b629afc123e2c93b12a01ad191926e8cb26317df
Olaleye Blessing <Olayinkablexxy@gmail.com>
2024-12-12 21:47:39 +0100

    Make Mongodb url dynamic

--- server/src/dbs/mongodb.ts
+++ server/src/dbs/mongodb.ts
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
+import { envVars } from '../utils/env-data';
 
 export const connectMongoDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/crowdchain');
+    await mongoose.connect(envVars.MONGO_URL);
     console.log('✅ Mongo DB connection successful');
   } catch (error) {
     console.log('Error connecting to mongoDB', error);
